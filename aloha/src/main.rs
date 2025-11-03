9f1068ec4767d012b7b7c8b458138428b37da01a
kedihacker <68711696+kedihacker@users.noreply.github.com>
2025-11-03 18:36:47 +0300

    first

--- aloha/src/main.rs
+++ aloha/src/main.rs
@@ -1,3 +1,26 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret = rand::random_range(1..=100);
+
+    println!("The secret number is: {}", secret);
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    loop {
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let parsed = guess.trim().parse::<u32>().expect("not a number");
+        match parsed.cmp(&secret) {
+            std::cmp::Ordering::Equal => println!("you winn"),
+            std::cmp::Ordering::Greater => println!("too big"),
+            std::cmp::Ordering::Less => println!("too small"),
+        }
+        guess = "".to_string();
+    }
 }
