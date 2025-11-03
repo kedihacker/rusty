43017774da8fd60382913a5f1c5243bcc3529661
kedihacker <68711696+kedihacker@users.noreply.github.com>
2025-11-03 19:06:50 +0300

    wasmtime

--- aloha/src/main.rs
+++ aloha/src/main.rs
@@ -1,26 +1,58 @@
-use std::io;
+use wasmtime::*;
 
-fn main() {
-    println!("Guess the number!");
+struct MyState {
+    name: String,
+    count: usize,
+}
 
-    let secret = rand::random_range(1..=100);
+fn main() -> Result<()> {
+    // First the wasm module needs to be compiled. This is done with a global
+    // "compilation environment" within an `Engine`. Note that engines can be
+    // further configured through `Config` if desired instead of using the
+    // default like this is here.
+    println!("Compiling module...");
+    let engine = Engine::default();
+    let module = Module::from_file(&engine, "examples/hello.wat")?;
 
-    println!("The secret number is: {}", secret);
+    // After a module is compiled we create a `Store` which will contain
+    // instantiated modules and other items like host functions. A Store
+    // contains an arbitrary piece of host information, and we use `MyState`
+    // here.
+    println!("Initializing...");
+    let mut store = Store::new(
+        &engine,
+        MyState {
+            name: "hello, world!".to_string(),
+            count: 0,
+        },
+    );
 
-    println!("Please input your guess.");
+    // Our wasm module we'll be instantiating requires one imported function.
+    // the function takes no parameters and returns no results. We create a host
+    // implementation of that function here, and the `caller` parameter here is
+    // used to get access to our original `MyState` value.
+    println!("Creating callback...");
+    let hello_func = Func::wrap(&mut store, |mut caller: Caller<'_, MyState>| {
+        println!("Calling back...");
+        println!("> {}", caller.data().name);
+        caller.data_mut().count += 1;
+    });
 
-    let mut guess = String::new();
+    // Once we've got that all set up we can then move to the instantiation
+    // phase, pairing together a compiled module as well as a set of imports.
+    // Note that this is where the wasm `start` function, if any, would run.
+    println!("Instantiating module...");
+    let imports = [hello_func.into()];
+    let instance = Instance::new(&mut store, &module, &imports)?;
 
-    loop {
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-        let parsed = guess.trim().parse::<u32>().expect("not a number");
-        match parsed.cmp(&secret) {
-            std::cmp::Ordering::Equal => println!("you winn"),
-            std::cmp::Ordering::Greater => println!("too big"),
-            std::cmp::Ordering::Less => println!("too small"),
-        }
-        guess = "".to_string();
-    }
+    // Next we poke around a bit to extract the `run` function from the module.
+    println!("Extracting export...");
+    let run = instance.get_typed_func::<(), ()>(&mut store, "run")?;
+
+    // And last but not least we can call it!
+    println!("Calling export...");
+    run.call(&mut store, ())?;
+
+    println!("Done.");
+    Ok(())
 }
